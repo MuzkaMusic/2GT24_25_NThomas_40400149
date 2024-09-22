1497304846ab0e93983170ca166155ef8ad4d9e4
MuzkaMusic <nikmuzka@gmail.com>
2024-09-22 14:49:48 +0100

    added camera tracking

--- Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
+public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     //We'll move the vehicle forward    
-     transform.Translate(Vector3.forward*Time.deltaTime *speed);
+        // Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + new Vector3(0,5,-7);
     }
 }
